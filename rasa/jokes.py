52b01515d5985960ba15f184b2ac405cd6b65993
Iambc123 <98503866+Iambc123@users.noreply.github.com>
2022-06-10 13:13:18 +0530

    Add initial rasa moodbot files

---
+++ rasa/jokes.py
@@ -0,0 +1,28 @@
+'''import requests
+
+def Joke( ): 
+    api_address='https://v2.jokeapi.dev/joke/Any'
+    url = api_address
+
+    response = requests.get(url)
+    response = response.json()
+
+    if(response['type']=='twopart'):
+        format_add = response['setup'] + '\n'+response['delivery']  
+    else:
+        format_add = response['joke']     
+
+    return format_add'''
+import requests    
+api_address='https://v2.jokeapi.dev/joke/Any'
+url = api_address
+
+response = requests.get(url)
+response = response.json()
+
+if(response['type']=='twopart'):
+    format_add = response['setup'] + '\n'+response['delivery']  
+else:
+    format_add = response['joke']     
+
+print(type(format_add)) 
